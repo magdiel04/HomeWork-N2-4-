83be925908551a3c3d9bc1425ee7da7ffc770ac5
Magdiel <magdiel.altynbekov@gmail.com>
2022-12-30 02:18:17 +0600

    Initial Commit

---
+++ HomeWork N2(4)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork N2(4)
+//
+//  Created by Magdiel Altynbekov on 30/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HomeWork N2(4)UITests/HomeWork_N2_4_UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HomeWork_N2_4_UITestsLaunchTests.swift
+//  HomeWork N2(4)UITests
+//
+//  Created by Magdiel Altynbekov on 30/12/22.
+//
+
+import XCTest
+
+final class HomeWork_N2_4_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
